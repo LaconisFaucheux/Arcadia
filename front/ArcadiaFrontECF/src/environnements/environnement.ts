91ba2dbea1add2689e40327d0e83861d186960ae
Nicolas Frotey <nicolas.frotey@gmail.com>
2024-11-12 13:25:43 +0100

    test

--- front/ArcadiaFrontECF/src/environnements/environnement.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: false,
-  apiUrl: 'https://localhost:7015/api',
-  imageApiUrl: 'https://localhost:7015/',
-  meteoApiUrl: 'https://api.meteo-concept.com/api/forecast/daily/0?latlng=48.01667%2C-2.18333&world=false&token=9e815bdf2765cdaefabd8fdfc7595ec23021cf1e09c90ce581240737cd0ce745'
-};
